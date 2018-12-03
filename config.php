cca779b7fdc913c51a6f7f6fcdbda8fc8205ac00
armadi21 <45292106+armadi21@users.noreply.github.com>
2018-12-03 23:46:25 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
+//jangan ada sepasi
+$config = [
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4eZutCBarWlmc28oHugiYm6lYLMeWCziIqdhaO3lX56iNCwqolttM6Gkq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=c6491883dc2c7e6f",
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBio-YrpiJaL-4hpa9o45tf3aulYPPgpm_q4qekaO4zH6dkM-v0KSiv85_yq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=c6491883dc2c7e6f",
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBioOXr-CkorOTh82yfXukiYm2lI7MdpyziIFnkYqv3YqHi5auqn6itMuCkq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=c6491883dc2c7e6f",
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4_csLqJsLOTgpWxepivfobJy465hp3AeH6dhI28z3-Ki9yumJKltLVylq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=c6491883dc2c7e6f",
+
+
+ ];
+
+?>
